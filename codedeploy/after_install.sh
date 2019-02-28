e88c58ff77f7b319292c30207dcae297b6244c29
Bruno Pereira <bruno.pereira@bluebatgames.com>
2019-02-28 11:22:23 -0800

    test

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # This script is executed after the source is copied to the instances
-export COMPOSER_HOME="$HOME/.config/composer"
-cd /var/www/test-deploy
-composer install
+# export COMPOSER_HOME="$HOME/.config/composer"
+# cd /var/www/test-deploy
+# composer install
