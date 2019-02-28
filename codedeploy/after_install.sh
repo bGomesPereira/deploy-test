42934743da38651b40bed0238e3351e58aa8f66a
Bruno Pereira <bruno.pereira@bluebatgames.com>
2019-02-28 12:08:41 -0800

    test

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -2,5 +2,6 @@
 
 # This script is executed after the source is copied to the instances
 HOME="/home/centos"
+export COMPOSER_HOME="/home/centos/.config/composer";
 cd /var/www/test-deploy
 composer install
